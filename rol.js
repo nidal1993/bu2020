ecd1b94fe9c7568ed53a7807be8f194d11cbadeb
nidal1993 <55952984+nidal1993@users.noreply.github.com>
2019-10-02 07:41:19 +0300

    Add files via upload

---
+++ rol.js
@@ -0,0 +1,89 @@
+/*القائمة العلوية */
+var rol1url = "http://www.business6service.com/2019/09/blog-post.html";
+var rol1img = "https://1.bp.blogspot.com/-iNKRNYL5J6Q/XXAgPF3RT0I/AAAAAAAAAKw/gJiT_L9iQc4dDTJJT7_e6zjtywqf9YT3gCLcBGAs/s320/hqdefault.jpg";
+var rol1title = "فلم البدلة الرسمية";
+var rol2url = "http://www.business6service.com/2019/08/blog-post_13.html";
+var rol2img = "https://i.ytimg.com/vi/PH91W7RYkBY/0.jpg";
+var rol2title = "التضخم";
+var rol3url = "http://www.business6service.com/2019/08/2019-hd-aladdin.html";
+var rol3img = "https://i.ytimg.com/vi/ChemYKKuNxE/0.jpg";
+var rol3title = "علاء الدين";
+var rol4url = "http://www.business6service.com/2019/08/blog-post_40.html";
+var rol4img = "https://i.ytimg.com/vi/psKZ4a-LAKA/0.jpg";
+var rol4title = "قوة الأمواج";
+var rol5url = "http://www.business6service.com/2019/08/898.html";
+var rol5img = "https://1.bp.blogspot.com/-It0-khOZ7tc/XV7t4jMGB1I/AAAAAAAAAKc/DI5_2tcFzVEFA7bKW9js4ShhjTpClV0JwCEwYBhgL/s400/%25D8%25A7%25D9%2584%25D8%25AA%25D9%2582%25D8%25A7%25D8%25B7.JPG";
+var rol5title = "ون بيس898";
+/*القائمة الجانبية*/
+var rol6img="https://i.ytimg.com/vi/psKZ4a-LAKA/0.jpg";
+var rol61url = "http://www.business6service.com/2019/08/blog-post_40.html";var rol61title = "قوة الأمواج";
+var rol62url = "http://www.business6service.com/2019/08/blog-post_40.html";var rol62title = "قوة الأمواج";
+var rol63url = "http://www.business6service.com/2019/08/blog-post_40.html";var rol63title = "قوة الأمواج";
+var rol64url = "http://www.business6service.com/2019/08/blog-post_40.html";var rol64title = "قوة الأمواج";
+var rol65url = "http://www.business6service.com/2019/08/blog-post_40.html";var rol65title = "قوة الأمواج";
+var rol66url = "http://www.business6service.com/2019/08/blog-post_40.html";var rol66title = "قوة الأمواج";
+var rol67url = "http://www.business6service.com/2019/08/blog-post_40.html";var rol67title = "قوة الأمواج";
+var rol68url = "http://www.business6service.com/2019/08/blog-post_40.html";var rol68title = "قوة الأمواج";
+var rol69url = "http://www.business6service.com/2019/08/blog-post_40.html";var rol69title = "قوة الأمواج";
+/*اعلان الاول*/
+var ads300250="<h1>nidal</h1>";
+/*الاعلان الثاني*/
+var ads3002502="<h1>abubaker</h1>";
+/*القائمة السفلية الخلفية للشائعة*/
+var urlmenu1="http://www.business6service.com/2019/08/2019-hd-aladdin.html"; var titlemenu1="علاء الدين";
+var urlmenu2="http://www.business6service.com/2019/08/blog-post_40.html"; var titlemenu2="قوة الأمواج";
+var urlmenu3="https://www.google.com/"; var titlemenu3="google";
+var urlmenu4="https://www.google.com/"; var titlemenu4="فلم شاروخان";
+var urlmenu5="https://www.google.com/"; var titlemenu5="google";
+var urlmenu6="https://www.google.com/"; var titlemenu6="google";
+/*لاضافة بندجديد للقائمة*/
+var urlliaa="https://"; var titleliaa="العنف";
+var liaa="<li role='menuitem'><a href='"+urlliaa+"' title='"+titleliaa+"'>"+titleliaa+"</a></li>";
+
+/*اعلان الاول*/
+var ads73890="<h1>nidal</h1>";
+
+
+/*كود آخر اعلان او عير ذلك*/
+var sld1="<h1>nidal</h1>";
+
+
+/*الفوتر القائمة*/
+var url_footertx1="1";var title_footertx1="سياسة الخصوصية";
+var url_footertx2="2";var title_footertx2="التعليمات";
+var url_footertx3="3";var title_footertx3="اتصل بنا";
+/*الفوتر الحقوق*/
+var footerla1="جميع الحقوق محفوظة للمبرمج نضال أبوبكر";
+
+
+/*mmmmmmmmmmmmmmmmmmmmmmmm*/
+  /*الرئيسية*/
+ var urlmenuitema1="#";var titlemenuitema1="الرئيسية";
+var urlmenuitema2="#";var titlemenuitema2="الأقسام";
+var urlmenuitema3="#";var titlemenuitema3="سياسة الخصوصية";
+var urlmenuitema4="#";var titlemenuitema4="التعليمات";
+var urlmenuitema5="#";var titlemenuitema5="اتصل بنا";
+ /*قسم الأقسام*/
+var urlmenuitemb1="#";var titlemenuitemb1="الأفلام";
+var urlmenuitemb2="#";var titlemenuitemb2="الفيديوهات";
+var urlmenuitemb3="#";var titlemenuitemb3="المسرحيات";
+var urlmenuitemb4="#";var titlemenuitemb4="السياسة والاقتصاد";
+var urlmenuitemb5="#";var titlemenuitemb5="الكرتون";
+var urlmenuitemb6="#";var titlemenuitemb6="قصص وروايات";
+ /*قسم الأفلام*/
+var urlmenuitemc1="#";var titlemenuitemc1="مصرية";
+var urlmenuitemc2="#";var titlemenuitemc2="أجنبية";
+var urlmenuitemc3="#";var titlemenuitemc3="تركية";
+ /*قسم الفيديوهات*/
+var urlmenuitemd1="#";var titlemenuitemd1="ثقافي";
+var urlmenuitemd2="#";var titlemenuitemd2="كوميدي";
+var urlmenuitemd3="#";var titlemenuitemd3="طبخ";
+/*قسم المسرحيات*/
+var urlmenuiteme1="#";var titlemenuiteme1="مصرية";
+var urlmenuiteme2="#";var titlemenuiteme2="خليجية";
+var urlmenuiteme3="#";var titlemenuiteme3="فلسطينية";
+ /*قسم الاقتصاد والسياسة*/
+var urlmenuitemh1="#";var titlemenuitemh1="مراحل المشروع ونموه";
+var urlmenuitemh2="#";var titlemenuitemh2="مشاريع صغيرة";
+var urlmenuitemh3="#";var titlemenuitemh3="الأسهم والسندات";
+/*mmmmmmmmmmmmmmmmmmmmmmmm*/
